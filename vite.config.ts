7d2d95d016d0d67a4718ceb8667ffcb59aaf1aca
Abhinav Patta <abhinav.patta01@gmail.com>
2026-05-30 12:24:58 +0530

    Add GitHub Pages auto-redirect hack and commit dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/NEON-SHADOW-CYBER-RUNNER/',
+  base: './',
   plugins: [react(), tailwindcss()],
 })
